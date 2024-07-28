c68f8c134ae77104ab5fb9127775e0b7cfbf196f
Mahmoud Reda <mahmoudreda457@gmail.com>
2024-07-28 11:18:14 +0300

    require typesense

--- config/scout.php
+++ config/scout.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Todo;
+
 return [
 
     /*
@@ -154,24 +156,24 @@ return [
     'typesense' => [
         'client-settings' => [
             'api_key' => env('TYPESENSE_API_KEY', 'xyz'),
-            'nodes' => [
+            'nodes'   => [
                 [
-                    'host' => env('TYPESENSE_HOST', 'localhost'),
-                    'port' => env('TYPESENSE_PORT', '8108'),
-                    'path' => env('TYPESENSE_PATH', ''),
+                    'host'     => env('TYPESENSE_HOST', 'localhost'),
+                    'port'     => env('TYPESENSE_PORT', '8108'),
+                    'path'     => env('TYPESENSE_PATH', ''),
                     'protocol' => env('TYPESENSE_PROTOCOL', 'http'),
                 ],
             ],
             'nearest_node' => [
-                'host' => env('TYPESENSE_HOST', 'localhost'),
-                'port' => env('TYPESENSE_PORT', '8108'),
-                'path' => env('TYPESENSE_PATH', ''),
+                'host'     => env('TYPESENSE_HOST', 'localhost'),
+                'port'     => env('TYPESENSE_PORT', '8108'),
+                'path'     => env('TYPESENSE_PATH', ''),
                 'protocol' => env('TYPESENSE_PROTOCOL', 'http'),
             ],
-            'connection_timeout_seconds' => env('TYPESENSE_CONNECTION_TIMEOUT_SECONDS', 2),
+            'connection_timeout_seconds'   => env('TYPESENSE_CONNECTION_TIMEOUT_SECONDS', 2),
             'healthcheck_interval_seconds' => env('TYPESENSE_HEALTHCHECK_INTERVAL_SECONDS', 30),
-            'num_retries' => env('TYPESENSE_NUM_RETRIES', 3),
-            'retry_interval_seconds' => env('TYPESENSE_RETRY_INTERVAL_SECONDS', 1),
+            'num_retries'                  => env('TYPESENSE_NUM_RETRIES', 3),
+            'retry_interval_seconds'       => env('TYPESENSE_RETRY_INTERVAL_SECONDS', 1),
         ],
         'model-settings' => [
             // User::class => [
